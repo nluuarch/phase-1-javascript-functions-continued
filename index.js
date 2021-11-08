4b2ac6e9aed2682e5f0257747903d052988a4a10
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-08 13:25:38 -0800

    commit tree f5b5a68843e2a7706fa83bc506fe6aaf3da2fabd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(mondayActivity = "go to the office") {
+    return `This Monday, I will ${mondayActivity}.`
+};
+
+// function wrapAdjective(emphasis) {
+//     const innerFunction = function(adj = "a hard worker"){
+//         return `You are ${emphasis}${adj}${emphasis}!"
+//     }
+//     return innerFunction;
+// };
+
+function wrapAdjective(emphasis = "*") {
+    const innerFunction = function(adj = "a hard worker"){
+        return `You are ${emphasis}${adj}${emphasis}!`;
+    }
+    return innerFunction;
+};
+
